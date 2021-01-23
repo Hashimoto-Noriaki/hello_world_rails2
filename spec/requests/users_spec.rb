ee9478bea70a061a61b1bc822e0d258f8889c083
橋本範昭 <nori.hasimo.89@gmail.com>
2021-01-24 03:48:35 +0900

    エラー対応

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -4,12 +4,9 @@ RSpec.describe "Users", type: :request do
   describe "GET /users" do
     subject{ get(users_path)}
     before {create_list(:user,3)}
-
     it "ユーザーの一覧が取得できる" do
-  # get users_path
+  #  get users_path
       subject
-
-     
     res = JSON.parse(response.body)
     expect(res.length).to eq 3
     expect(res[0].keys).to eq ["id", "account", "name", "email", "created_at", "updated_at"]
@@ -19,7 +16,22 @@ RSpec.describe "Users", type: :request do
   end
 
   describe  "GET/users/:id" do
-    it "任意のユーザーのレコードが取得できる" do
+    subject { get(user_path(user_id))}
+  
+    context "指定したidのユーザーが存在するとき" do
+    let(:user_id){ user.id}
+    let(:user) { create(:user) }
+
+    fit "そのユーザーのレコードが取得できる" do
+      subject
+      binding.pry
+      expect(response).to have_http_status(200)
+   end
+   end
+    
+    context "指定したidのユーザーが存在しない時" do
+      it "ユーザーが見つからない" do
+      end
     end
   end
 
@@ -37,7 +49,5 @@ RSpec.describe "Users", type: :request do
     it "任意のユーザーのレコードを削除できる" do
     end
   end
-
-
 
 end
