ecad7bae1f8eaa78982f8a473d45cbefcb819dd9
橋本範昭 <nori.hasimo.89@gmail.com>
2021-01-23 12:46:54 +0900

    未コミットをコミットする

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :user do
-    
+    name{Faker::name}
+    account{Faker::Internet.username}
+    email{Faker::Internet.email}
   end
 end
+
+
+
+
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   context "accountを指定している時" do
     it "ユーザーが作られる" do
-      user = User.new(name: "pon",account: "pon",email: "pon@example.com")
-      # expect(user.valid?).to eq true
+      user = build(:user)
       expect(user).to be_valid 
     end
   end
-  # binding.pry
+
   context "accountを指定していない時" do
     it "ユーザー作成に失敗する" do
-      user = User.new(name: "pon",email: "pon@example.com")
+      user = build(:user,account: nil )
+      # user = User.new(name: "pon",email: "pon@example.com")
       expect(user).to be_invalid 
       expect(user.errors.details[:account][0][:error]).to eq :blank
     end
@@ -26,9 +26,11 @@ RSpec.describe User, type: :model do
 
 
   context "すでに同じ名前のaccountが存在している時" do
-    it "ユーザー作成に失敗する" do
-    User.create!(name: "pon",account: "pon",email: "pon@example.com")
-    user = User.new(name: "pochi",account: "pon",email: "pochi@example.com")
+     fit "ユーザー作成に失敗する" do
+    create(:user,account:"pon")
+      # User.create!(name: "pon",account: "pon",email: "pon@example.com")
+    user = build(:user,account:"pon")
+      # user = User.new(name: "pochi",account: "pon",email: "pochi@example.com")
     expect(user).to be_invalid
     expect(user.errors.details[:account][0][:error]).to eq :taken
     
