981269abf533d0054073b165dd020ab3ce8855e1
橋本範昭 <nori.hasimo.89@gmail.com>
2021-01-31 13:18:04 +0900

    usersテーブルを壊したことがきっかけでエラーが起きたが解決

--- db/migrate/20210107160312_create_users.rb
+++ db/migrate/20210107160312_create_users.rb
@@ -1,11 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
-    def change
-      create_table :users do |t|
-        t.string :name
-        t.string :account
-        t.string :email
-        t.timestamps
-      end
+      def change
+        create_table :users do |t|
+          t.string :name
+          t.string :account
+          t.string :email
+    
+          t.timestamps
+        end
+      end
     end
-  end
-  
+    
--- db/migrate/20210130160942_add_user_id_to_articles.rb
+++ db/migrate/20210130160942_add_user_id_to_articles.rb
@@ -1,5 +1,6 @@
 class AddUserIdToArticles < ActiveRecord::Migration[6.1]
-    def change
-      add_reference :articles, :user, null: false, foreign_key: true
+      def change
+        add_reference :articles, :user, null: false, foreign_key: true
+      end
     end
-  end
+    
